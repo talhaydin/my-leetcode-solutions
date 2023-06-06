aa461aa2a0d1e6d81dbbe316f6a3db01e54106f2
talhaydin <mt.aydin@outlook.com>
2023-06-07 00:16:13 +0300

    1768 explanation

--- 1768. Merge Strings Alternately/MergeStringsAlternatively.js
+++ 1768. Merge Strings Alternately/MergeStringsAlternatively.js
@@ -5,10 +5,14 @@
  */
 var mergeAlternately = function (word1, word2) {
   let result = "";
+  // below code first determines how many times it's going to run by using math.max on word 1 and word 2. then, by running two, non-connected if statements at the same time, ensures that the letters are added in an alternating way because the statements will always work one after another.
 
   for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
     if (i < word1.length) result += word1[i];
     if (i < word2.length) result += word2[i];
   }
+
+  // eventually the statement which adds the shorter word's letter will stop and the other one which is longer, will continue.
+
   return result;
 };
