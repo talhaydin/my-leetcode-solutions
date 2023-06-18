407d0037b1903d59b0c9229a84bc1011f02c89ef
talhaydin <mt.aydin@outlook.com>
2023-06-18 23:29:38 +0300

    internalization problem

---
+++ Other Solutions/Localization.js
@@ -0,0 +1,3 @@
+// Problem: Using fully front-end bound internalization techniques such as i18next is heavy on the workload of developer.
+
+// Problem 2: Using back-end bound internalization techniques such as getting resources from an api call is not as dynamic as expected.
