1b61a48ec8ac45f75982051bfbce087a568055f7
talhaydin <mt.aydin@outlook.com>
2023-06-20 23:53:04 +0300

    best time to buy and sell stock

--- 121. 121. Best Time to Buy and Sell Stock.js
+++ 121. 121. Best Time to Buy and Sell Stock.js
@@ -6,21 +6,3 @@ You want to maximize your profit by choosing a single day to buy one stock and c
 
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
  */
-
-/**
- * @param {number[]} prices
- * @return {number}
- */
-var maxProfit = function (prices) {
-  if (prices == null || prices.length <= 1) return 0; //empty array
-  let minBuy = prices[0]; //initialize min price to buy
-  let profit = 0; // max profit initialized to zero
-  for (let i = 1; i < prices.length; i++) {
-    //checks whole array
-    minBuy = Math.min(minBuy, prices[i]); //get min price
-    console.log(minBuy);
-    profit = Math.max(profit, prices[i] - minBuy); //get profit
-    console.log("profit" + profit);
-  }
-  return profit; //return max profit
-};
