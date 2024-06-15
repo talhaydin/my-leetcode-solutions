27b6faeeaef521da6bde3469d1fc3e02055bf922
talhaaydin <mt.aydin@outlook.com>
2024-06-15 23:09:49 +0300

    Best Time to Buy and Sell Stock

--- 121. Best Time to Buy and Sell Stock.js
+++ 121. Best Time to Buy and Sell Stock.js
@@ -11,3 +11,21 @@ Return the maximum profit you can achieve from this transaction. If you cannot a
  * @param {number[]} prices
  * @return {number}
  */
+var maxProfit = function (prices) {
+  // initialize the maxProfit and minPrice
+  let maxProfit = 0; // max profit is initialized zero because we don't know it yet.
+  let minPrice = prices[0]; // minPrice is initialized as the first element in array because we will keep updating it if we see smaller number in the rest of the array.
+
+  for (let i = 1; i < prices.length; i++) {
+    //loop over the array starting from second element
+    const currentPrice = prices[i]; // current price is whatever element we are at right now
+    const currentProfit = currentPrice - minPrice; //current profit is our number - min price
+    if (currentProfit > maxProfit) {
+      maxProfit = currentProfit; //if current profit is bigger than 0 initialized maxprofit, set maxProfit to our current profit
+    }
+    if (currentPrice < minPrice) {
+      minPrice = currentPrice; //if our current number is smaller than minPrice, set minPrice to our number. since this runs last in stack, the next loop will correctly find the maximum amount of profit we can make
+    }
+  }
+  return maxProfit;
+};
